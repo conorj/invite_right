fac057e8ebc607a9f6ff341f7946286ae578dc50
Conor Ryan <conor.ryan1302@gmail.com>
2017-04-10 00:56:25 +0100

    Add invite status validations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,7 @@
 class Invitation < ApplicationRecord
   belongs_to :event
   validates :unique_uri, uniqueness: true
+  validates :accepted, numericality: { only_integer: true }
+  validates :declined, numericality: { only_integer: true }
+  validates :tentative, numericality: { only_integer: true }
 end
